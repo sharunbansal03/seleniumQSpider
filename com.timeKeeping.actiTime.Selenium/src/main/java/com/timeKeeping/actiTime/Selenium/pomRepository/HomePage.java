d2324a4de0cb641c10672224980408bf3133ae81
sharunbansal03 <sharun.bansal03@gmail.com>
2022-09-19 18:18:55 +0530

    added webdriverwait in homepage.class

--- com.timeKeeping.actiTime.Selenium/src/main/java/com/timeKeeping/actiTime/Selenium/pomRepository/HomePage.java
+++ com.timeKeeping.actiTime.Selenium/src/main/java/com/timeKeeping/actiTime/Selenium/pomRepository/HomePage.java
@@ -1,9 +1,13 @@
 package com.timeKeeping.actiTime.Selenium.pomRepository;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
 	WebDriver driver;
@@ -14,7 +18,9 @@ public class HomePage {
 		this.driver = driver;
 	}
 	
-
+	@FindBy(id="preInsertedTransformedMoireId")
+	private WebElement hiddenInterceptingElement;
+	
 	@FindBy(id="container_tasks")
 	private WebElement taskButton;
 	
@@ -38,7 +44,11 @@ public class HomePage {
 		logoutLink.click();
 	}
 	
-	public void clickOnTaskButton() {
+	public void clickOnTaskButton() throws InterruptedException {
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		wait.until(ExpectedConditions.invisibilityOfElementLocated((By.id(
+				  "preInsertedTransformedMoireId"))));
+		
 		taskButton.click();
 	}
 }

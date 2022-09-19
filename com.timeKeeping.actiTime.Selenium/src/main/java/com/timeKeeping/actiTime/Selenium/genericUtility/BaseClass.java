d2da094fedff0d92cc60ee2e5f177ca85146d2e9
sharunbansal03 <sharun.bansal03@gmail.com>
2022-09-19 17:43:08 +0530

    updated pom.xml

--- com.timeKeeping.actiTime.Selenium/src/main/java/com/timeKeeping/actiTime/Selenium/genericUtility/BaseClass.java
+++ com.timeKeeping.actiTime.Selenium/src/main/java/com/timeKeeping/actiTime/Selenium/genericUtility/BaseClass.java
@@ -4,14 +4,12 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
 
 import com.timeKeeping.actiTime.Selenium.pomRepository.CreateNewCustomerPage;
 import com.timeKeeping.actiTime.Selenium.pomRepository.HomePage;
